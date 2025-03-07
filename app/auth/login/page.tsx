6dce9ea7a315635e9f8e440a388f1889ef62eb36
JBartrop <116983357+JBartrop@users.noreply.github.com>
2025-03-07 14:27:51 +0000

    touches

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import Input from "../../components/input";
 import Link from "next/link";
+import { HiEyeSlash } from "react-icons/hi2";
+import { FaEye, FaKey, FaUser } from "react-icons/fa6";
 
 
 
@@ -13,7 +15,10 @@ export async function LoginAction() {
 const Login: React.FC = () => {
 
 
-    // const issubmitting = useState<boolean>(false)
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const [submitting, isSubmitting] = useState<boolean>(false)
+
+
     const [inputdata, setinputdata] = useState({
         email : "",
         password : ""
@@ -29,6 +34,8 @@ const Login: React.FC = () => {
     }
 
     const handleSave = () => {
+        isSubmitting(true);
+        new Promise(res=> setTimeout(res, Math.random() * 1000))
         console.log("Login Data:", inputdata);
         toast.success("login successfully!");
         localStorage.setItem("emergencyResponseProfile", JSON.stringify([inputdata]) )
@@ -42,20 +49,41 @@ const Login: React.FC = () => {
         <div className="bg-amber-50 rounded-lg p-8     w-10/12   sm:w-1/2   lg:w-2/5  ">
             
             <form method="POST" action="/">
-            <h1 className="text-center uppercase font-bold text-2xl ">login</h1>
-            <label>Email</label>
-            <Input type="email" name="email" placeholder="enter email" value={inputdata.email} onChange={handleChange} />
-            <label>Password</label>
-            <Input type="password" name="password" placeholder="enter password" value={inputdata.password} onChange={handleChange} />
-            <Link href="#">Forgot password?</Link>
+            <h1 className="text-center uppercase font-bold text-2xl text-black ">login</h1>
+
+            <label className="text-gray-800">Email</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                  <FaUser  />
+                </button>
+                <Input type="email" name="email" placeholder="enter email" value={inputdata.email} onChange={handleChange} />
+            </div>
+            
+
+            <label className="text-gray-800">Password</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                  <FaKey  />
+                </button>
+                
+                <Input type={showPassword ? "text" : "password"} name="password" placeholder="enter password" value={inputdata.password} onChange={handleChange} />
+                
+                <button type="button" onClick={() => setShowPassword(prev => !prev)} className="absolute inset-y-0 right-0 flex items-center pr-6 text-sm text-gray-600" aria-label={showPassword ? 'Hide password' : 'Show password'}>
+                {showPassword ? < HiEyeSlash /> : <FaEye /> }
+                </button>
+            </div>
+
+
+            <Link href="#" className="text-red-600">Forgot password?</Link>
             <button
                 type="button"
                 onClick={handleSave}
-                className="bg-red-300 text-black px-4 py-3 rounded-full hover:bg-red-400 active:bg-red-500 cursor-pointer duration-300 w-full "
-                // disabled={issubmitting}
-            >Login
+                className="bg-red-300 text-black my-4 px-4 py-3 rounded-full hover:bg-red-400 active:bg-red-500 cursor-pointer duration-300 w-full "
+                disabled={submitting}
+            >
+                {submitting ? "Logging in" : "Login"}
             </button>
-            <p>Don't have an account? <Link href="#">Register</Link> </p>
+            <p className="text-gray-800">Don't have an account? <Link href="/auth/register" className="text-red-600">Register</Link> </p>
             </form>
             
         </div>
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import Input from "../../components/input";
+import Link from "next/link";
+import { HiEyeSlash } from "react-icons/hi2";
+import { FaEye, FaKey, FaMailchimp, FaNoteSticky, FaPhone, FaUser } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 
 
@@ -12,7 +16,10 @@ export async function RegisterAction() {
 const Register: React.FC = () => {
 
 
-    // const issubmitting = useState<boolean>(false)
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    const [submitting, isSubmitting] = useState<boolean>(false)
+
+
     const [inputdata, setinputdata] = useState({
         firstName : "",
         lastName : "",
@@ -32,6 +39,8 @@ const Register: React.FC = () => {
     }
 
     const handleSave = () => {
+        isSubmitting(true);
+        new Promise(res=> setTimeout(res, Math.random() * 1000))
         console.log("Register Data:", inputdata);
         toast.success("created profile successfully!");
         localStorage.setItem("emergencyResponseProfile", JSON.stringify([inputdata]) )
@@ -49,33 +58,70 @@ const Register: React.FC = () => {
         <div className="bg-amber-50 rounded-lg p-8     w-10/12   sm:w-1/2   lg:w-2/5  ">
             
             <form method="POST" action="/">
-            <h1 className="text-center uppercase font-bold text-2xl ">Register</h1>
+            <h1 className="text-center uppercase font-bold text-2xl text-black ">Register</h1>
 
-            <label>First name</label>
-            <Input type="text" name="firstName" placeholder="enter first name" value={inputdata.firstName} onChange={handleChange} />
+            <label className="text-gray-800">First name</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                    <FaUser />
+                </button>
+                <Input type="text" name="firstName" placeholder="enter first name" value={inputdata.firstName} onChange={handleChange} />
+            </div>
 
-            <label>Last name</label>
-            <Input type="text" name="lastName" placeholder="enter last name" value={inputdata.lastName} onChange={handleChange} />
+            <label className="text-gray-800">Last name</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                    <FaUser />
+                </button>
+                <Input type="text" name="lastName" placeholder="enter last name" value={inputdata.lastName} onChange={handleChange} />
+            </div>
 
-            <label>Service number</label>
-            <Input type="text" name="serviceNumber" placeholder="enter service number" value={inputdata.serviceNumber} onChange={handleChange} />
+            <label className="text-gray-800">Service number</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                    <FaNoteSticky />
+                </button>
+                <Input type="text" name="serviceNumber" placeholder="enter service number" value={inputdata.serviceNumber} onChange={handleChange} />
+            </div>
 
-            <label>Telephone number</label>
-            <Input type="text" name="telephoneNumber" placeholder="enter email" value={inputdata.telephoneNumber} onChange={handleChange} />
+            <label className="text-gray-800">Telephone number</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                    <FaPhone />
+                </button>
+                <Input type="text" name="telephoneNumber" placeholder="enter email" value={inputdata.telephoneNumber} onChange={handleChange} />
+            </div>
 
-            <label>Email</label>
-            <Input type="text" name="email" placeholder="entertelephone number" value={inputdata.email} onChange={handleChange} />
+            <label className="text-gray-800">Email</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                  <MdEmail  />
+                </button>
+                <Input type="email" name="email" placeholder="enter email" value={inputdata.email} onChange={handleChange} />
+            </div>
 
-            <label>Password</label>
-            <Input type="password" name="password" placeholder="enter password" value={inputdata.password} onChange={handleChange} />
+            <label className="text-gray-800">Password</label>
+            <div className="relative">
+                <button className="absolute inset-y-0 left-0 flex items-center pl-6 text-sm text-gray-600">
+                  <FaKey  />
+                </button>
+                
+                <Input type={showPassword ? "text" : "password"} name="password" placeholder="enter password" value={inputdata.password} onChange={handleChange} />
+                
+                <button type="button" onClick={() => setShowPassword(prev => !prev)} className="absolute inset-y-0 right-0 flex items-center pr-6 text-sm text-gray-600" aria-label={showPassword ? 'Hide password' : 'Show password'}>
+                {showPassword ? < HiEyeSlash /> : <FaEye /> }
+                </button>
+            </div>
 
             <button
                 type="button"
                 onClick={handleSave}
-                className="bg-red-300 text-black px-4 py-3 rounded-md hover:bg-red-400 active:bg-red-500 cursor-pointer duration-300 w-full "
-                // disabled={issubmitting}
-            >Create Account
+                className="bg-red-300 my-4 text-black px-4 py-3 rounded-full hover:bg-red-400 active:bg-red-500 cursor-pointer duration-300 w-full "
+                disabled={submitting}
+            >
+                {submitting ? "Creating account" : "Create Account" }
             </button>
+            <p className="text-gray-800">Already have an account? <Link href="/auth/login" className="text-red-600">Login</Link> </p>
             </form>
             
         </div>
