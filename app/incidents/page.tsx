b81cc19374c98e19f7a6ba94ca5d0ecb584164e0
JBartrop <116983357+JBartrop@users.noreply.github.com>
2025-03-11 17:02:50 +0000

    mysideDone

---
+++ app/incidents/page.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Table,  TableBody,  TableCaption,   TableCell,   TableHead,   TableHeader,   TableRow } from "../../components/ui/table"
+
+
+
+
+
+interface TableProps{
+    reportNumber: string;
+    Location:string;
+    status:string;
+    date: string;
+};
+
+interface TableHeadProps{
+    id:string;
+    title:string;
+};
+
+const tableHead: TableHeadProps[] = [
+    {
+        id:"1",
+        title:"REPORT NUMBER"
+    },
+    {
+        id:"2",
+        title:"LOCATION"
+    },
+    {
+        id:"3",
+        title:"STATUS"
+    },
+    {
+        id:"4",
+        title:"DATE"
+    }
+]
+
+
+const invoices: TableProps[]  = [
+    {
+        reportNumber: "10020111",
+        Location: "Accra, Ghana",
+        status: "Resolved",
+        date: "09/08/2024",
+    },
+    {
+        reportNumber: "10005612",
+        Location: "Cape Coast, Ghana",
+        status: "Resolved",
+        date: "25/08/2024",
+    },
+    {
+        reportNumber: "10300223",
+        Location: "Tema, Ghana",
+        status: "Pending",
+        date: "12/06/2024",
+    },
+    {
+        reportNumber:"14033314",
+        Location: "Dzorwulu, Ghana",
+        status: "Ongoing",
+        date: "17/04/2024",
+    },
+    {
+       reportNumber: "15334615",
+        Location: "Georgia",
+        status: "Ongoing",
+        date: "17/01/2024",
+    },
+    {
+        reportNumber: "16043116",
+        Location: "Kumasi, Ghana",
+        status: "Resolved",
+        date: "15/02/2024",
+    },
+  ]
+
+
+const Incidents: React.FC = () => {
+    return (
+        <section className=" ml-50 p-16 min-h-screen">
+            <Table className="w-10/12 m-auto">
+                <TableCaption>A list of incidents happening.</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    {tableHead.map((heads) => (
+                        <TableHead className="first:w-[200px] last:text-right ">{heads.title}</TableHead>
+                    ))}
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {invoices.map((invoice) => (
+                    <TableRow key={invoice.reportNumber}>
+                      <TableCell className="font-medium">{invoice.reportNumber}</TableCell>
+                      <TableCell>{invoice.Location}</TableCell>
+                      <TableCell>{invoice.status}</TableCell>
+                      <TableCell className="text-right">{invoice.date}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+            </Table>
+        </section>
+    )
+}
+
+export default Incidents;
---
+++ app/personnel/page.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { Table,  TableBody,  TableCaption,   TableCell,   TableFooter,   TableHead,   TableHeader,   TableRow } from "../../components/ui/table"
+
+interface TableProps{
+    serviceNumber: string;
+    name:string;
+    email:string;
+    phoneNumber: string;
+};
+
+interface TableHeadProps{
+    id:string;
+    title:string;
+};
+
+const tableHead: TableHeadProps[] = [
+    {
+        id:"1",
+        title:"SERVICE NUMBER"
+    },
+    {
+        id:"2",
+        title:"NAME"
+    },
+    {
+        id:"3",
+        title:"EMAIL"
+    },
+    {
+        id:"4",
+        title:"TELEPHONE NUMBER"
+    }
+]
+
+
+const invoices: TableProps[]  = [
+    {
+        serviceNumber: "10000111",
+        name: "Sarpong",
+        email: "sarpong@gmail.com",
+        phoneNumber: "0203778989",
+    },
+    {
+        serviceNumber: "10005611",
+        name: "Kwaku",
+        email: "Kwaku@gmail.com",
+        phoneNumber: "0203778989",
+    },
+    {
+        serviceNumber: "10000222",
+        name: "Mafia",
+        email: "Mafia@gmail.com",
+        phoneNumber: "0245778989",
+    },
+    {
+        serviceNumber: "10033311",
+        name: "Killua",
+        email: "Killua@gmail.com",
+        phoneNumber: "0203669989",
+    },
+    {
+        serviceNumber: "10334611",
+        name: "Georgia",
+        email: "Georgia@gmail.com",
+        phoneNumber: "0243686989",
+    },
+    {
+        serviceNumber: "10043111",
+        name: "Ayanakoji",
+        email: "Ayanakoji@gmail.com",
+        phoneNumber: "0267878989",
+    },
+  ]
+
+const Personnel: React.FC = () => {
+    return (
+        <section className=" ml-50 p-16 min-h-screen w-screen">
+            <Table className="w-10/12 m-auto">
+                <TableCaption>A list of your service personnels.</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    {tableHead.map((heads) => (
+                        <TableHead className="first:w-[200px] last:text-right ">{heads.title}</TableHead>
+                    ))}
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {invoices.map((invoice) => (
+                    <TableRow key={invoice.serviceNumber}>
+                      <TableCell className="font-medium">{invoice.serviceNumber}</TableCell>
+                      <TableCell>{invoice.name}</TableCell>
+                      <TableCell>{invoice.email}</TableCell>
+                      <TableCell className="text-right">{invoice.phoneNumber}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                {/* <TableFooter>
+                  <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                  </TableRow>
+                </TableFooter> */}
+            </Table>
+        </section>
+    )
+}
+
+export default Personnel;
